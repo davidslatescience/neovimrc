ebef82a71b215072425645d98f84eda746d50e66
Dave <david@slatescience.com>
2025-10-17 14:56:00 +1100

    refactor: extract LSP keymap setup into helper function
    - Add `setup_lsp_keymaps` function in helpers.lua
    - Replace inline LSP keymap setup with helper function call in remap.lua

--- lua/dgrogan/remap.lua
+++ lua/dgrogan/remap.lua
@@ -61,27 +61,7 @@ vim.keymap.set("n", "<F1>", ":lua vim.diagnostic.open_float(0, {scope=\"line\"})
 -- LSP keymaps (set on LspAttach)
 vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-    callback = function(ev)
-        vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
-        local opts = { buffer = ev.buf }
-        vim.keymap.set('n', 'gD', ":vsplit | TSToolsGoToSourceDefinition<CR>", opts)
-        vim.keymap.set('n', 'gd', "TSToolsGoToSourceDefinition", opts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-        vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-        vim.keymap.set('n', '<space>wl', function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, opts)
-        vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-        vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<space>f', function()
-            vim.lsp.buf.format { async = true }
-        end, opts)
-    end,
+    callback = helpers.setup_lsp_keymaps,
 })
 
 -- ============================================================================
